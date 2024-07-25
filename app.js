1bf028eb3034a36e48be205ed264b90a4ec4b7ef
pinjunpan <pinjunpann@gmail.com>
2024-07-25 15:59:11 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('express app for movies')
+  res.redirect('/movies') //重新導向，首頁顯示電影清單
+})
+
+app.get('/movies', (req, res) => {
+  res.send('listing movies')
+})
+
+app.get('/movie/:id', (req, res) => {
+  const id = req.params.id //params設動態路由
+  res.send(`read movie: ${id}`)
 })
 
 app.listen(port, () => {
