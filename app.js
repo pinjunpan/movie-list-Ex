9e9c4b04669a68759292493a9ae7eea3df526ac5
pinjunpan <pinjunpann@gmail.com>
2025-05-01 15:59:11 +0800

    add pagination

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express')
 const {engine} = require('express-handlebars')
 const app = express()
 const port = 3000
-const movies=require('./public/jsons/movies.json').results
+const movies = require('./public/jsons/movies.json').results
 const BASE_IMG_URL = 'https://movie-list.alphacamp.io/posters/'
+const helpers = require('./helpers/handlebars-helper')
 
-app.engine('.hbs', engine({extname: '.hbs'}))
+app.engine('.hbs', engine({extname: '.hbs', helpers}))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(express.static('public')) //載入靜態檔案
@@ -16,6 +17,10 @@ app.get('/', (req, res) => {
 
 app.get('/movies', (req, res) => {
   const keyword = req.query.search?.trim()
+  const page = Number(req.query.page) || 1
+  const MOVIES_PER_PAGE = 8
+
+  //篩選關鍵字
   const matchedMovies = keyword? movies.filter((mv) =>
     Object.values(mv).some((property) =>{
       if(typeof property === 'string'){
@@ -24,7 +29,23 @@ app.get('/movies', (req, res) => {
       return false
     })
   ):movies
-  res.render('index', {movies: matchedMovies, BASE_IMG_URL, keyword})
+
+  //分頁
+  const totalPages = Math.ceil(matchedMovies.length / MOVIES_PER_PAGE)
+  const pageMovies = matchedMovies.slice((page - 1) * MOVIES_PER_PAGE, page * MOVIES_PER_PAGE)
+
+  const pages = []
+  for (let i = 1; i <= totalPages; i++){
+    pages.push(i)
+  }
+  
+  res.render('index', {
+    movies: pageMovies,
+    BASE_IMG_URL,
+    keyword,
+    currentPage: page,
+    pages
+  })
 })
 
 app.get('/movie/:id', (req, res) => {
